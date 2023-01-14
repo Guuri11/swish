fc5e99c42c631b7ddd2730510a3d267579af3224
Sergio Gurillo <sergio.gurillo11@gmail.com>
2023-01-14 14:02:17 +0100

    first commit

---
+++ src/main/java/com/swish/app/SwishApplication.java
@@ -0,0 +1,13 @@
+package com.swish.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwishApplication.class, args);
+	}
+
+}
