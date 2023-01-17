76f2cbf601c7499c3d8c9eba980c84c8cb7ccb7b
Sergio Gurillo <sergio.gurillo11@gmail.com>
2023-01-17 18:00:19 +0100

    add auth to swagger

---
+++ src/main/java/com/swish/app/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.swish.app.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI customizeOpenAPI() {
+
+    final String securitySchemeName = "bearerAuth";
+    return new OpenAPI()
+        .addSecurityItem(new SecurityRequirement()
+            .addList(securitySchemeName))
+        .components(new Components()
+            .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                .name(securitySchemeName)
+                .type(Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")));
+  }
+}
