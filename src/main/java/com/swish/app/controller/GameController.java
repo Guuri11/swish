825c323d27eeea7ef1d049706c6bda97169ef76e
Sergio Gurillo <sergio.gurillo11@gmail.com>
2023-01-17 17:53:05 +0100

    Add delete endpoint to Game, Team & User

--- src/main/java/com/swish/app/controller/GameController.java
+++ src/main/java/com/swish/app/controller/GameController.java
@@ -6,6 +6,7 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,6 +54,14 @@ public class GameController {
     return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF)
             .toUri())
         .body(entityModel);
+  }
+
+  @DeleteMapping("/{id}")
+  ResponseEntity<?> deleteGame(@PathVariable final Long id) {
+
+    gameService.delete(id);
+    return ResponseEntity.noContent()
+        .build();
   }
 
 }
