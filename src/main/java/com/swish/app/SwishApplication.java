87234d198672d74864a54eeb801c012d9eda2f3c
Sergio Gurillo <sergio.gurillo11@gmail.com>
2023-01-18 10:29:32 +0100

    cors conf

--- src/main/java/com/swish/app/SwishApplication.java
+++ src/main/java/com/swish/app/SwishApplication.java
@@ -2,12 +2,30 @@ package com.swish.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SwishApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SwishApplication.class, args);
-	}
+  public static void main(final String[] args) {
+
+    SpringApplication.run(SwishApplication.class, args);
+  }
+
+  @Configuration
+  public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+
+      registry.addMapping("/**")
+          .allowedOrigins("http://localhost:3000")
+          .allowedMethods("GET", "POST", "PUT", "DELETE")
+          .allowCredentials(true);
+    }
+  }
+
 
 }
